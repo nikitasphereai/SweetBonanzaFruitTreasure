85049a6c998f23e06617d715b5294615753e00bc
nikitasphereai <nikita.sphereai@gmail.com>
2024-12-01 22:06:09 +0700

    Initial Commit

---
+++ SweetBonanzaFruitTreasure/SweetBonanzaFruitTreasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetBonanzaFruitTreasureApp.swift
+//  SweetBonanzaFruitTreasure
+//
+//  Created by GRIDFLOW65@GMAIL.COM on 01.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SweetBonanzaFruitTreasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
